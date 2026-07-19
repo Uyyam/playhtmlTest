b8db9a6bd68c4b3d28aef74ca4be233ffc761b02
Uyyam <124654782+Uyyam@users.noreply.github.com>
2026-07-19 12:02:20 -0400

    Enable user creation and video capture functionality
    Uncomment createUser function and related code for user creation and video capture.

--- script.js
+++ script.js
@@ -1,5 +1,49 @@
 
 //createUser();
+
+
+
+
+// const presences = playhtml.presence.getPresences();
+// for (const [id, p] of presences) {
+//   p.isMe;            // boolean
+//   p.playerIdentity;  // name, colors, publicKey
+//   p.cursor;          // { x, y, pointer } | null
+// // createNewCap(p.playerIdentity);
+// }
+// console.log(presences);
+
+// Subscribe to a specific channel — fires only when that channel changes
+//const unsub = playhtml.presence.onPresenceChange("playerIdentity", createUser);
+
+//when new user joins
+
+function createUser(){
+  const el = document.createElement("div");
+  el.setAttribute("can-mirror", "")
+el.id = playhtml.presence.getMyIdentity().publicKey;
+//
+document.getElementById('sketch-holder').appendChild(el);
+new p5(newCapture);
+//playhtml.setupPlayElement(el, { ignoreIfAlreadySetup: true });
+}
+
+function newCapture(p) {
+  p.setup = function () {
+  let canvas = p.createCanvas(100, 200);
+let capture = p.createCapture(p.VIDEO,{ flipped:true });
+  
+  // Move the canvas so it’s inside our <div id="sketch-holder">.
+
+    capture.parent(document.getElementById(playhtml.presence.getMyIdentity().publicKey));
+          canvas.parent('sketch-holder');
+          canvas.hide();
+  };
+}
+
+
+
+//createUser();
 let capture;
 let canvas;
 
